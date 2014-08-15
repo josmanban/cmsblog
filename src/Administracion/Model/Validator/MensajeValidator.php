0cbc0181c77c04982c944b8c1961f97c4eac8e54
José Manuel Banda <josemanuelbanda@gmail.com>
2014-08-15 06:27:26 -0300

    implementacion mensajes entre usuarios

---
+++ src/Administracion/Model/Validator/MensajeValidator.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Administracion\Model\Validator;
+
+use Librerias\Validator;
+use Librerias\Conexion;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ValidatorUsuario
+ *
+ * @author jose
+ */
+class PerfilValidator extends Validator {
+
+    //put your code here
+
+    function __construct($entity = null) {
+        $this->entity = $entity;
+    }
+
+    public function validate() {
+        try {
+            $this->validateEmptyFields();
+            $this->validateSpecialFields();
+            $this->cleanFields();
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    protected function validateEmptyFields() {
+        if (isset($_FILES['avatar']) && $_FILES['avatar']['name'] != '')
+            $this->addError(self::validateImage('avatar', 'avatar', 90000));
+        $this->checkErrores();
+    }
+
+    protected function validateSpecialFields() {
+        $this->addError($this->validateNullProperty($this->entity->getUsuario(), 'usuario'));
+        $this->checkErrores();
+    }
+
+    public function cleanFields() {
+        $this->entity->setAvatar(self::clean($this->entity->getAvatar()));
+        $this->entity->getDescripcion(self::clean($this->entity->getDescripcion()));
+    }
+
+}
+
+?>
